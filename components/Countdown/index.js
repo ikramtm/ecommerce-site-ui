099b127911e2aceda27fb2d5460a4ccca799cff6
Ikram Mustapha <imustapha@nascentdigital.com>
2021-02-23 14:04:24 +0800

    feat: add timer to flash sales

---
+++ components/Countdown/index.js
@@ -0,0 +1,27 @@
+import Countdown from 'react-countdown'
+import styles from '../../styles/Home.module.scss'
+
+const Completionist = () => <span>You are good to go!</span>
+
+const renderer = ({ formatted: { hours, minutes, seconds } }) => {
+    return (
+      <span className={styles.timer}>
+        <div className={styles.timer__display}>{hours}</div>
+        <div className={styles.timer__display}>{minutes}</div>
+        <div className={styles.timer__display}>{seconds}</div>
+      </span>
+    )
+}
+
+const TimerCountdown = ({ time }) => {
+  return (
+    <Countdown
+      date={Date.now() + (time)}
+      zeroPadTime={2}
+      renderer={renderer}
+      precision={2}
+    />
+  )
+}
+
+export default TimerCountdown
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Nav from '../components/Nav/index'
 import Hero from '../components/Hero/index'
 import ProductCard from '../components/ProductCard/index'
 import Voucher from '../components/Voucher/index'
+import TimerCountdown from '../components/Countdown/index'
 
 import styles from '../styles/Home.module.scss'
 import { getInventory, getSalesVouchers } from '../services/stocks'
@@ -20,16 +21,20 @@ const Home = ({ flashSale, vouchers }) => {
       <Nav />
       <Hero />
       <main className={styles.main}>
-        Flash Sales
-        <Grid container spacing={5}>
+        <div className={styles.flashSales}>
+          <img className={styles.flashSales__icon} src='/flash.svg' alt='' />
+          <p className={styles.flashSales__title}>Flash Sales</p>
+          <TimerCountdown time={20000}/>
+        </div>
+        <div className={styles.gridz}>
           { flashSale.map((product) => {
             return (
-              <Grid item xs={12} md={3}>
-                <ProductCard key={product.name} {...product} />
-              </Grid>
+              // <Grid item xs={12} md={3}>
+              <ProductCard key={product.name} {...product} />
+              // </Grid>
             )
           })}
-        </Grid>
+        </div>
         <div className={styles.vouchers}>
           <Grid container spacing={2}>
             {vouchers.map((voucher) => {
